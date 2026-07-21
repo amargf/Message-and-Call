d65b7b658b53fc682c204d98732b61ac48178a1f
amargf <americainstore89@gmail.com>
2026-07-21 19:35:10 +0100

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://mslhnlrpdhrffezjdwyf.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zbGhubHJwZGhyZmZlempkd3lmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ2NDA1MjEsImV4cCI6MjEwMDIxNjUyMX0._Nx1aw_Wk1bZy0i_35lkIc6dTUbXLerqQ599wSNAqZY';
+const supabaseUrl = 'https://avtdbnenosrqufmfcqrf.supabase.co';
+const supabaseAnonKey = 'sb_publishable_Bh-TgvAAYAcEUMTEbvP1Kg_PSbrHrjG';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
